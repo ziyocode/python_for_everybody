64f70eb6a6ed7b635c37249730f863de298504bc
ziyocode <ziyocode@users.noreply.github.com>
2019-03-25 22:52:14 +0900

    chapter_3 exercises update

---
+++ chapter03/chained_conditionals.py
@@ -0,0 +1,18 @@
+x = 3
+y = 3
+
+if x < y :
+    print("x is less than y")
+elif x > y :
+    print("x is greater than y")
+else :
+    print("x and y are equal")
+
+choice = 'c'
+
+if choice == 'a' :
+    print("Bad guess")
+elif choice == 'b' :
+    print("Good guess")
+elif choice == 'c' :
+    print("Close, but not correct")
---
+++ chapter03/nested_conditionals.py
@@ -0,0 +1,10 @@
+x = 3
+y = 4
+
+if x == y :
+    print("x and y equal")
+else :
+    if x < y :
+        print("x is less than y")
+    else :
+        print("x is greater than y")
